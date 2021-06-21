c972d8b706b85c84013fb2ee1886f2f1dc163770
RootAndroid58 <rootand58@gmail.com>
2021-06-22 01:15:47 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -17,6 +17,17 @@ hmdty= api_data['main']['humidity']
 wind_spd = api_data['wind']['speed']
 date_time = datetime.now().strftime('%d %b %Y | %I:%M:%S %p')
 
+f= open("weather.txt","w+")
+f.write("Project by Rohit Mahajan\n")
+f.write("-------------------------------------------------------------------\n")
+f.write("warther stats for - {} || {} \n".format(location.upper(), date_time))
+f.write("----------------------------------------\n")
+f.write("current temp is      : {:.2f} deg C \n".format(temp_city))
+f.write("current weather desc : "+weather_desc+"\n")
+f.write("current Humidity     : "+str(hmdty)+"%\n")
+f.write("current Wind Speed   : "+str(wind_spd)+" kmph\n")
+f.close()
+
 print("----------------------------------------")
 print('warther stats for - {} || {}'.format(location.upper(), date_time))
 print('----------------------------------------')
